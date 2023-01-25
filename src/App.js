080ed253537fb3faee4dda6752b25121a158e647
Sanjar Saparov <117997805+sanzarr0704@users.noreply.github.com>
2023-01-25 11:37:26 +0600

    Это дз 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+incrementCount() {
+  this.setState((state) => {
+    return {count: state.count + 1}
+  });
 }
+
+handleSomething() = {
+  {this.incrementCount()};
+  {this.incrementCount()};
+  {this.incrementCount()};
+
+ 
+};
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <img className="Avatar"
+//           src={props.author.avatarUrl}
+//           alt={props.author.name}
+//         />
+//         <div className="UserInfo-name">
+//           {props.author.name}
+//         </div>
+//       </div>
+//       <div className="Comment-text">
+//         {props.text}
+//       </div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// } 
+
 
 export default App;
